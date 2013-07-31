6de19155e730a4cb4eda271d566c7895939f6617
Justin Hall <hall5714@vandals.uidaho.edu>
2013-07-31 14:38:18 -0700

    fix(demo): partial fix to demo template

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     ngversion: '1.0.5',
-    bsversion: '2.3.1',
+    bsversion: '3.0.0-rc1',
     modules: [],//to be filled in by build task
     pkg: grunt.file.readJSON('package.json'),
     dist: 'dist',
